3f2890122cd0f834ab2be2a0dbf26054f400e67c
Makoto WAKAMATSU <makoto@j01.itscom.net>
2015-09-24 12:26:37 +0900

    Restored the star

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,7 +1,9 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("+"), DEAD_CELL("."); private String symbol;
+    LIVE_CELL("*"), DEAD_CELL(".");
+    
+    private String symbol;
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
